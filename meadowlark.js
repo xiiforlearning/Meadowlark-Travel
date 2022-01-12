69fa6186fb34be543154bb793550e8359f846e17
Otabek Azamov <otabek_azamov@epam.com>
2022-01-12 12:46:49 +0500

    init: add initial files

---
+++ meadowlark.js
@@ -0,0 +1,47 @@
+import express from "express";
+import { engine } from "express-handlebars";
+
+const app = express();
+
+app.use(express.static("public"));
+
+const port = process.env.PORT || 3000;
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+const fortunes = [
+  "Победи свои страхи, или они победят тебя.",
+  "Рекам нужны истоки.",
+  "Не бойся неведомого.",
+  "Тебя ждет приятный сюрприз.",
+  "Будь проще везде, где только можно.",
+];
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render("about", { fortune: randomFortune });
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.render("404");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(500);
+  res.render("500");
+});
+
+app.listen(port, () =>
+  console.log(
+    `Express запущен на http://localhost:${port}; ` +
+      `нажмите Ctrl+C для завершения.`
+  )
+);
